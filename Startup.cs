9a949dc663208e95aca97326592ed2b29d496d2d
Pavel Osokin <pachkaosokin@yandex.ru>
2024-09-23 13:13:28 +0500

    настройка Startup

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -11,30 +12,53 @@ using System.Threading.Tasks;
 namespace ПР49_Осокин
 {
     public class Startup
-
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Авторизация",
+                    Description = "Авторизация и регистрация пользователей"
+                });
+                c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "Версии",
+                    Description = "Получение списка версий"
+                });
+                c.SwaggerDoc("v3", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v3",
+                    Title = "Блюда",
+                    Description = "Получение блюд из базы"
+                });
+                c.SwaggerDoc("v4", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v4",
+                    Title = "Заказы",
+                    Description = "Отправка заказов и получение истории заказов"
+                });
+                //var filePath = Path.Combine(System.AppContext.BaseDirectory, "Практическая_49_Тепляков.xml");
+                //c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Авторизация");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "Версии");
+                c.SwaggerEndpoint("/swagger/v3/swagger.json", "Блюда");
+                c.SwaggerEndpoint("/swagger/v4/swagger.json", "Заказы");
             });
         }
     }
